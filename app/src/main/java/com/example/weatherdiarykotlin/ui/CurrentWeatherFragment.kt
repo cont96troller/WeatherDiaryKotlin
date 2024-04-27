2783ca21617314d75c3b5b1e50621691d958ccfa
gggmg <cont96roller@gmail.com>
2024-04-27 20:22:52 +0900

    Activity, Fragment, ViewModel 클래스에 hilt annotation 추가

--- app/src/main/java/com/example/weatherdiarykotlin/ui/CurrentWeatherFragment.kt
+++ app/src/main/java/com/example/weatherdiarykotlin/ui/CurrentWeatherFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.weatherdiarykotlin.databinding.FragmentWeatherCurrentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class CureentWeatherFragmnet : Fragment() {
+@AndroidEntryPoint
+class CurrentWeatherFragment : Fragment() {
     private lateinit var binding:FragmentWeatherCurrentBinding
 
     override fun onCreateView(
