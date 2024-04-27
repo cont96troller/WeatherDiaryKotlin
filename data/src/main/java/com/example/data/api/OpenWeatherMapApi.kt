1228d00b673ee098d40c04c1ab077598d12575b9
gggmg <cont96roller@gmail.com>
2024-04-27 19:43:25 +0900

    api 호출 interface 클래스 생성

---
+++ data/src/main/java/com/example/data/api/OpenWeatherMapApi.kt
@@ -0,0 +1,19 @@
+package com.example.data.api
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface OpenWeatherMapApi {
+    companion object {
+        fun create(baseUrl: String): OpenWeatherMapApi {
+            return createRetrofit(baseUrl, OpenWeatherMapApi::class.java)
+        }
+    }
+
+    @GET("data/2.5/weather")
+    suspend fun getCurrentWeather(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("apiKey") apiKey: String
+    ): CurrentWeatherResponse
+}
