6508c3ab4c7d204e0d0abbd8b531f42b3da921e9
gggmg <cont96roller@gmail.com>
2024-04-27 20:05:22 +0900

    hilt NetworkModule, RepositoryModule 생성

---
+++ app/src/main/java/com/example/weatherdiarykotlin/di/NetworkModule.kt
@@ -0,0 +1,19 @@
+package com.example.weatherdiarykotlin.di
+
+import com.example.data.api.OpenWeatherMapApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOpenWeatherMapApi(): OpenWeatherMapApi {
+        return OpenWeatherMapApi.create("https://api.openweathermap.org")
+    }
+}
---
+++ app/src/main/java/com/example/weatherdiarykotlin/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.example.weatherdiarykotlin.di
+
+import com.example.data.api.OpenWeatherMapApi
+import com.example.data.repository.OpenWeatherMapRepositoryImpl
+import com.example.domain.repository.OpenWeatherMapRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideOpenWeatherMapRepository(openWeatherMapApi: OpenWeatherMapApi): OpenWeatherMapRepository =
+        OpenWeatherMapRepositoryImpl(openWeatherMapApi)
+}
