a21ae975745f215f2a3b8610bc10070f3247e033
gggmg <cont96roller@gmail.com>
2024-04-29 21:54:02 +0900

    Adapter, ViewHolder 추가

---
+++ app/src/main/java/com/example/weatherdiarykotlin/WeatherListAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.weatherdiarykotlin
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.domain.model.ListWeather
+import com.example.weatherdiarykotlin.databinding.ItemWeatherListBinding
+
+class WeatherListAdapter: RecyclerView.Adapter<WeatherListAdapter.ViewHolder>() {
+
+    private var datalist = mutableListOf<ListWeather>()
+
+    class ViewHolder(private val binding: ItemWeatherListBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(listWeather:ListWeather) {
+            binding.txtWeatherStatus.text = listWeather.description
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding=ItemWeatherListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int=datalist.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(datalist[position])
+    }
+}
+
