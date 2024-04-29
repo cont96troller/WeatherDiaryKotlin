300b93e2fe1ccd0aca6ab0667ca115c9ede65f43
gggmg <cont96roller@gmail.com>
2024-04-29 23:05:41 +0900

    날씨 리스트 api 호출 interface 추가

--- data/src/main/java/com/example/data/api/OpenWeatherMapApi.kt
+++ data/src/main/java/com/example/data/api/OpenWeatherMapApi.kt
@@ -1,6 +1,7 @@
 package com.example.data.api
 
 import com.example.data.model.CurrentWeatherResponse
+import com.example.data.model.ListWeatherResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -17,4 +18,12 @@ interface OpenWeatherMapApi {
         @Query("lon") lon: Double,
         @Query("apiKey") apiKey: String
     ): CurrentWeatherResponse
+
+    @GET("data/2.5/forecast/daily")
+    suspend fun getListWeather(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("cnt") cnt: Int,
+        @Query("apikey") apiKey: String
+    ): ListWeatherResponse
 }
