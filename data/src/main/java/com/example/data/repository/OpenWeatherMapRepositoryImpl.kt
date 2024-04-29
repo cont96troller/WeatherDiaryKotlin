fafd65830812c5c5f7a4797726802d7d5d001ba6
gggmg <cont96roller@gmail.com>
2024-04-29 23:23:54 +0900

    data 영역 RepositoryImpl getListWeather 추가, WeatherListResponse 추가

--- data/src/main/java/com/example/data/repository/OpenWeatherMapRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/OpenWeatherMapRepositoryImpl.kt
@@ -3,7 +3,9 @@ package com.example.data.repository
 import com.example.data.BuildConfig
 import com.example.data.api.OpenWeatherMapApi
 import com.example.data.model.CurrentWeatherResponse
+import com.example.data.model.ListWeatherResponse
 import com.example.domain.model.CurrentWeather
+import com.example.domain.model.ListWeather
 import com.example.domain.repository.OpenWeatherMapRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -19,6 +21,19 @@ class OpenWeatherMapRepositoryImpl(private val api: OpenWeatherMapApi) : OpenWea
                 api.getCurrentWeather(lat, lon, apiKey = BuildConfig.WEATHER_API_KEY)
             val description = apiResult.weather.first().description
             emit(CurrentWeather(description = description))
+        }
+    }
+
+    override fun getListWeather(
+        lat: Double,
+        lon: Double,
+        cnt: Int,
+        apiKey: String
+    ): Flow<ListWeather> {
+        return flow {
+            val apiResult: ListWeatherResponse =
+                api.getListWeather(lat, lon, cnt, apiKey = BuildConfig.WEATHER_API_KEY)
+            //TODO data
         }
     }
 }
