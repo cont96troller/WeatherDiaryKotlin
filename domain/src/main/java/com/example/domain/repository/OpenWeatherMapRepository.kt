95bc892dd006146f81a02c516be6801faedca485
gggmg <cont96roller@gmail.com>
2024-04-29 23:21:32 +0900

    domain 영역 Repository interface에 getListWeather 추가

--- domain/src/main/java/com/example/domain/repository/OpenWeatherMapRepository.kt
+++ domain/src/main/java/com/example/domain/repository/OpenWeatherMapRepository.kt
@@ -1,10 +1,12 @@
 package com.example.domain.repository
 
 import com.example.domain.model.CurrentWeather
+import com.example.domain.model.ListWeather
 import kotlinx.coroutines.flow.Flow
 
 
 interface OpenWeatherMapRepository {
 
     fun getCurrentWeather(lat: Double, lon: Double, apiKey: String): Flow<CurrentWeather>
+    fun getListWeather(lat: Double, lon: Double, cnt: Int, apiKey: String): Flow<ListWeather>
 }
